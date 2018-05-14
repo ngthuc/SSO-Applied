e9bd14cc5e064d8bff4533a006b2dc93d51fdab4
tuyetnghi <39264637+tuyetnghi@users.noreply.github.com>
2018-05-14 19:28:31 +0700

    Upload

--- CT263/lib/dbCon.php
+++ CT263/lib/dbCon.php
@@ -5,6 +5,6 @@
 	$database = "shophtx2";
 	global $conn;
 	$conn = mysqli_connect($host, $user, $pass, $database) or die ("Could not connect: " . mysqli_error());
-	mysqli_query($conn,"SET NAMES 'utf8'");
+	mysqli_query($conn,"SET NAMES 'utf8'"); 
 //	$db = mysql_select_db($database, $conn) or die ("Could not select database");
 ?>
