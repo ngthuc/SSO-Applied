3fe1cd8fe3f99a7716d4724025bf3fa8d27cae26
Nguyen Thuc <lenguyenthuc.edu@gmail.com>
2018-05-14 18:10:16 +0700

    Remove module IoT

--- TSV2017-12/module_IoT/client_module/Webservices/source/config.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	$host = "localhost:3306";
-	$username = "root";
-	$password = "";
-	$datebase = "diemdanh_nckh_webservices";
-?>
--- TSV2017-12/module_IoT/client_module/Webservices/source/getTime.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	date_default_timezone_set("asia/bangkok");
-	if ($_GET['type']== "date"){
-		echo date("d-m-Y");
-	}	
-	else if ($_GET['type']== "time"){
-		echo date("H:i:s");
-	}	
-?>
