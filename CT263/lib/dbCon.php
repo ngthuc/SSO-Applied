28e6d0d3fd7b1702757c7e5da5956e6e8dfca5dc
Nguyen Thuc <lenguyenthuc.edu@gmail.com>
2018-05-14 18:58:17 +0700

    Update

--- CT263/lib/dbCon.php
+++ CT263/lib/dbCon.php
@@ -1,7 +1,7 @@
 <?php
 	$host = "localhost";
 	$user = "root";
-	$pass = "";
+	$pass = "mysql";
 	$database = "shophtx2";
 	global $conn;
 	$conn = mysqli_connect($host, $user, $pass, $database) or die ("Could not connect: " . mysqli_error());
