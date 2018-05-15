3efa8a8212af0be82f2a3a804db48989850e99c1
Nguyen Thuc <lenguyenthuc.edu@gmail.com>
2018-05-15 20:22:25 +0700

    Something code

--- SampleCart/index.php
+++ SampleCart/index.php
@@ -2,70 +2,72 @@
 session_start();
 ?>
 <html>
-<head>
- <title>Demo Shopping Cart - Created By My Kenny</title>
- <link rel="stylesheet" href="style.css" />
+  <head>
+    <title>Demo Shopping Cart</title>
+    <link rel="stylesheet" href="style.css" />
 
- <!-- Auth0 -->
- <script src="https://cdn.auth0.com/js/auth0/8.8/auth0.min.js"></script>
+    <!-- Auth0 -->
+    <script src="https://cdn.auth0.com/js/auth0/8.8/auth0.min.js"></script>
 
- <!-- Initializing Script -->
- <script>
-     $(document).ready(function() {
-      var webAuth = new auth0.WebAuth({
-         domain: 'tuyetnghi96.auth0.com',
-         clientID: '7Hr6oFjh6XJa4nkesvi3sbXclnXkIKjd',
-         redirectUri: 'http://localhost/sso/SSO_Lib_and_Callback/callback.php',
-         audience: `https://tuyetnghi96.auth0.com/userinfo`,
-         responseType: 'code',
-         scope: 'openid profile'
+    <!-- Initializing Script -->
+    <script>
+       $(document).ready(function() {
+        var webAuth = new auth0.WebAuth({
+           domain: 'tuyetnghi96.auth0.com',
+           clientID: '7Hr6oFjh6XJa4nkesvi3sbXclnXkIKjd',
+           redirectUri: 'http://localhost/sso/SSO_Lib_and_Callback/callback.php',
+           audience: `https://tuyetnghi96.auth0.com/userinfo`,
+           responseType: 'code',
+           scope: 'openid profile'
+         });
+
+        $('#login').click(function(e) {
+           e.preventDefault();
+           webAuth.authorize();
+         });
        });
-
-       $('#login').click(function(e) {
-         e.preventDefault();
-         webAuth.authorize();
-       });
-     });
- </script>
-</head>
-<body>
-<h1>Demo Shopping Cart</h1>
-<div id='cart'>
-<?php
-$ok=1;
- if(isset($_SESSION['cart']))
- {
-  foreach($_SESSION['cart'] as $k=>$v)
-  {
-   if(isset($v))
-   {
-   $ok=2;
-   }
-  }
- }
- if ($ok != 2)
-  {
-  echo '<p>Ban khong co mon hang nao trong gio hang</p>';
- } else {
-  $items = $_SESSION['cart'];
-  echo '<p>Ban dang co <a href="cart.php">'.count($items).' mon hang trong gio hang</a></p>';
- }
-?>
-</div>
-<?php
-include 'connect.php';
-$sql = "SELECT * FROM books ORDER BY id DESC";
-$query = mysql_query($sql);
-if(mysql_num_rows($query) > 0) {
- while($row = mysql_fetch_array($query)) {
-  echo "<div class=pro>";
-  echo "<h3>$row[title]</h3>";
-  echo "Tac Gia: $row[author] - Gia: ".number_format($row[price],3)." VND<br />";
-  echo "<p align='right'><a href='addcart.php?item=$row[id]'>Mua Sach Nay</a></p>";
-  echo "</div>";
- }
-}
-
-?>
-</body>
+   </script>
+  </head>
+  <body>
+  <center>
+    <h1>Demo Shopping Cart</h1>
+    <h4>
+      <button>
+        <a href="login.php" style="text-decoration: none">Đăng nhập</a>
+      </button>
+    </h4>
+    <div id='cart'>
+    <?php
+      $ok=1;
+      if(isset($_SESSION['cart'])) {
+        foreach($_SESSION['cart'] as $k=>$v) {
+           if(isset($v)) {
+             $ok=2;
+           }
+        }
+      }
+      if ($ok != 2) {
+        echo '<p>Ban khong co mon hang nao trong gio hang</p>';
+      } else {
+        $items = $_SESSION['cart'];
+        echo '<p>Ban dang co <a href="cart.php">'.count($items).' mon hang trong gio hang</a></p>';
+       }
+    ?>
+    </div>
+    <?php
+      include 'connect.php';
+      $sql = "SELECT * FROM books ORDER BY id DESC";
+      $query = mysql_query($sql);
+      if(mysql_num_rows($query) > 0) {
+        while($row = mysql_fetch_array($query)) {
+          echo "<div class=pro>";
+          echo "<h3>$row[title]</h3>";
+          echo "Tac Gia: $row[author] - Gia: ".number_format($row[price],3)." VND<br />";
+          echo "<p align='right'><a href='addcart.php?item=$row[id]'>Mua Sach Nay</a></p>";
+          echo "</div>";
+        }
+      }
+    ?>
+    </center>
+  </body>
 </html>
--- SampleCart/login.php
+++ SampleCart/login.php
@@ -1,1 +1,37 @@
-<button id="login" class="btn btn-lg btn-danger btn-block">Sign in to Home with Auth0</button>
+
+<!DOCTYPE html>
+<html lang="vi" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Demo Giỏ hàng | Đăng nhập</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <center>
+      <h1>Đăng nhập để mua hàng</h1>
+      <button type="button"><a href="index.php" style="text-decoration: none">Quay lại trang chủ</a></button>
+      <form method="post">
+        <table>
+          <tr>
+            <td>Tên đăng nhập</td>
+            <td><input type="text" name="uid" placeholder="Tài khoản"></td>
+          </tr>
+          <tr>
+            <td>Mật khẩu</td>
+            <td><input type="password" name="pwd" placeholder="Mật khẩu"></td>
+          </tr>
+          <tr>
+            <td><button type="reset" style="width: 100%">Nhập lại</button></td>
+            <td><button type="submit" name="login" style="background-color: blue; color: white; width: 100%">Đăng nhập</button></td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <hr>
+              <button type="button" id="login" style="background-color: red; color: white; width: 100%">Đăng nhập với Auth0</button>
+            </td>
+          </tr>
+        </table>
+      </form>
+    </center>
+  </body>
+</html>
