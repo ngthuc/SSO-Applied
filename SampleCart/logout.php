610946e69def8a9b89a58fec89f316351b32d223
Nguyen Thuc <lenguyenthuc.edu@gmail.com>
2018-07-30 06:57:06 +0700

    Upload demo pictures

--- SampleCart/logout.php
+++ SampleCart/logout.php
@@ -6,11 +6,13 @@ if(isset($_SESSION['user'])) {
   // if(isset($_COOKIE['userid'])){
   //     setcookie("userid", "", time()-3600);
   // }
+
+  echo '<script>alert("Đăng xuất thành công!")</script>';
   if(isset($_SESSION['type']) == 'SSO'){
     echo '<meta http-equiv="refresh" content="0,url=http://localhost/sso/swa/logout.php?return=http://localhost/sso/SampleCart">';
+  } else {
+    echo '<meta http-equiv="refresh" content="0,url=index.php">';
   }
-  echo '<script>alert("Đăng xuất thành công!")</script>';
-  echo '<meta http-equiv="refresh" content="0,url=index.php">';
 } else {
   echo '<meta http-equiv="refresh" content="0,url=index.php">';
 }
